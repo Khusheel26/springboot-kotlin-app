e731d04618d58f3aca1f1a6e0a4e17ee4eeec5e0
Khusheel26 <khusheel26@gmail.com>
2024-02-21 21:53:49 +0530

    Added the deafult files using spring initializer

---
+++ src/main/kotlin/com/octobit8/springbootkotlinapp/SpringbootKotlinAppApplication.kt
@@ -0,0 +1,11 @@
+package com.octobit8.springbootkotlinapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/octobit8/springbootkotlinapp/SpringbootKotlinAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.octobit8.springbootkotlinapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
